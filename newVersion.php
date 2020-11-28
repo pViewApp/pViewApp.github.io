67463b0e3d68aca9293a4d432cc4a79825e8ac8a
ProfessorShep <o0wang@outlook.com>
2020-11-28 18:36:10 -0500

    Update newVersion.php

--- newVersion.php
+++ newVersion.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "db.php";
-if ($_GET['secret'] == 'ShearSheP22') {
+if ($_GET['secret'] == '60f4be0abf96e6c9ef812136eeac5ff7eb6bfeda354284f97818edf122dcc965') {
     if (strpos($_GET['version'], "beta") == false && strpos($_GET['version'], "alpha") == false) {
         $stmt = $conn->prepare('INSERT INTO versions(version, displayText, downloadLink) VALUES (?, ?, ?)');
         $stmt->bind_param('sss', $_GET['version'], $_GET['displayText'], $_GET['downloadLink']);
